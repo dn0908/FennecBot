5d2fa5feb21b4a526cd47b9f1a99e5cf2befa27d
dn0908 <dahyun.diana.nam@gmail.com>
2023-08-21 20:51:54 +0900

    Yolo Updates... (yolo v5 running checked)

--- fpts_limit_rtsp.py
+++ fpts_limit_rtsp.py
@@ -1,27 +1,31 @@
 import time
 import cv2
 import pyzbar.pyzbar as pyzbar
+import torch
 
+# model = torch.load('./yolo_v5/best.pt')
+# device = torch.device('cpu')
+# print(device)
+# model.to(device)
 
 code_info : str= ""
-fpsLimit = 1 # limit
+fpsLimit = 0 # limitq
 startTime = time.time()
 cap = cv2.VideoCapture("rtsp:/192.168.2.2:8554/raw", cv2.CAP_FFMPEG)
 while True:
     ret, frame = cap.read()
     nowTime = time.time()
-    if (int(nowTime - startTime)) > fpsLimit:
+    if (nowTime - startTime) >= fpsLimit:
         
 
-        prev_code_info = code_info
 
+        # READ QR
+        prev_code_info = code_info
         codes = pyzbar.decode(frame)
         print(codes)
         for code in codes:
             code_info = code.data.decode('utf-8')
             print("QR code info : ", code_info)
-
-
         if code_info != prev_code_info:
             QR_toggle = 0
             break
---
+++ rtsp_yolo_test.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import torch
+import yaml
+import sys
+import time
+
+fpsLimit = 1 # limitq
+startTime = time.time()
+
+
+
+# Modify sys.path
+sys.path.insert(0, "/home/smi/FennecBot/fennecbot_v03_yolov5_tiny/yolov5")
+
+# Now import attempt_load
+from yolov5.models.experimental import attempt_load
+
+# Load the "custom" YOLOv5 model
+model = attempt_load('/home/smi/FennecBot/fennecbot_v03_yolov5_tiny/last_320.pt')
+
+# Initialize the webcam capture
+# webcam_cap = cv2.VideoCapture(0)
+webcam_cap = cv2.VideoCapture("rtsp:/192.168.2.2:8554/raw", cv2.CAP_FFMPEG)
+
+# Load class names from data.yaml
+with open('/home/smi/FennecBot/fennecbot_v03_yolov5_tiny/yolov5/FennecBot-Proto-1/data.yaml', 'r') as yaml_file:
+    data = yaml.safe_load(yaml_file)
+    class_names = data['names']
+
+while True:
+    # Capture the webcam frame
+    ret, webcam_frame = webcam_cap.read()
+    webcam_frame = cv2.resize(webcam_frame, (640, 480))
+
+    nowTime = time.time()
+    if (nowTime - startTime) >= fpsLimit:
+
+        # webcam_frame = cv2.resize(webcam_frame, (480, 640))
+        print(webcam_frame.shape)
+        
+        # Check if frame read is valid
+        if not ret:
+            print("Failed to grab frame.")
+            continue
+        
+            # Convert the webcam frame from BGR to RGB and reshape for model input
+        img = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2RGB)
+        img_tensor = torch.from_numpy(img).float().permute(2, 0, 1).unsqueeze(0) / 255.0
+        
+        # Pass the frame through the YOLOv5 model
+        results = model(img_tensor)
+
+        # Extract tensor from results tuple
+        detections = results[0]
+
+        # Assuming there's a confidence threshold you want to apply
+        conf_thresh = 0.05
+
+        # Use the confidence score to filter out weak detections
+        mask = detections[0, :, 4] > conf_thresh
+
+        # Extract the boxes, scores, and classes from the detections
+        boxes = detections[0, mask, :4].cpu().numpy()
+        scores = detections[0, mask, 4].cpu().numpy()
+        classes = detections[0, mask, 5].cpu().numpy().astype(np.int32)
+
+        # Draw the bounding boxes and labels on the frame
+        for box, score, class_idx in zip(boxes, scores, classes):
+            x1, y1, x2, y2 = map(int, box)
+            class_name = class_names[class_idx]
+            cv2.rectangle(webcam_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(webcam_frame, f"{class_name}: {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            
+            # Print the coordinates of the detected object
+            print(f"{class_name} coordinates: ({x1}, {y1}), ({x2}, {y2})")
+
+        startTime = time.time() # reset time
+
+    # Display the frame on the screen
+    print(webcam_frame.shape)
+
+    cv2.imshow('Webcam Capture', webcam_frame)
+
+    # Exit the program if the user presses the 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and close the window
+webcam_cap.release()
+cv2.destroyAllWindows()
